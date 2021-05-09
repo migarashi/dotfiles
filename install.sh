2598adcfa42247ebb967172a9ac1df4ffcbc0459
migarashi <62993587+migarashi@users.noreply.github.com>
2021-05-09 15:15:39 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ ln -fs $(pwd)/_zshrc ~/.zshrc
 cp _gitconfig ~/.gitconfig
 git config --global user.email $1
 
-mkdir -p ~/.zsh/completion
-cd $_
+mkdir -p ~/.zsh/completion && cd $_
 curl -LO https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
 curl -LO https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose
+
+cd ..
 curl -LO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
