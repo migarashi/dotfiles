9b24b82559b8111490606a5857ea9b285be0113b
migarashi <103835638+migarashi@users.noreply.github.com>
2022-06-15 12:03:14 +0900

    Update dotfiles

--- .zshrc
+++ .zshrc
@@ -8,5 +8,5 @@ FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
 autoload -Uz compinit
 compinit -i
 
-source "$(brew --prefix)/opt/zsh-git-prompt/zshrc.sh"
-PROMPT="%B%m%~%b$(git_super_status) %# "
+. "$(brew --prefix)"/opt/zsh-git-prompt/zshrc.sh
+PROMPT='%B%m%~%b$(git_super_status) %# '
--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/sh -eu
 
+defaults write com.apple.screencapture location ~/Downloads
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install \
+  go \
+  pwgen \
+  shellcheck \
+  shfmt \
   tfenv \
   zsh-completions \
   zsh-git-prompt
 
 etc=/Applications/Docker.app/Contents/Resources/etc
+ln -fs $etc/_docker "$(brew --prefix)"/share/zsh-completions/_docker
 ln -fs $etc/docker-compose.zsh-completion "$(brew --prefix)"/share/zsh-completions/_docker-compose
-ln -fs $etc/docker.zsh-completion "$(brew --prefix)"/share/zsh-completions/_docker
 
 cp .gitconfig ~
 git config --global user.email "$1"
@@ -17,4 +23,7 @@ ln -fs "$PWD"/.vimrc ~
 
 cp .zshrc ~
 tfenv install
+tfenv use latest
 terraform -install-autocomplete
+
+echo done!
