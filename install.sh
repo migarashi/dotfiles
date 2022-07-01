a4b38c37d88596e93a6bc3164bf9cda5fd0a0363
migarashi <103835638+migarashi@users.noreply.github.com>
2022-07-01 19:58:59 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -eu
 
+cp .gitconfig ~
+git config --global user.email "$1"
+
 defaults write com.apple.screencapture location ~/Downloads
+
+ln -fs "$PWD"/.vimrc ~
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install \
@@ -12,18 +17,13 @@ brew install \
   zsh-completions \
   zsh-git-prompt
 
-etc=/Applications/Docker.app/Contents/Resources/etc
-ln -fs $etc/_docker "$(brew --prefix)"/share/zsh-completions/
-ln -fs $etc/docker-compose.zsh-completion "$(brew --prefix)"/share/zsh-completions/_docker-compose
-
-cp .gitconfig ~
-git config --global user.email "$1"
-
-ln -fs "$PWD"/.vimrc ~
-
 cp .zshrc ~
 tfenv install
 tfenv use latest
 terraform -install-autocomplete
+
+etc=/Applications/Docker.app/Contents/Resources/etc
+ln -fs $etc/_docker "$(brew --prefix)"/share/zsh-completions/
+ln -fs $etc/docker-compose.zsh-completion "$(brew --prefix)"/share/zsh-completions/_docker-compose
 
 echo done!
