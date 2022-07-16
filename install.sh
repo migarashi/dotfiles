b51bf6313fff0220c6ba0d66666c819633fd9c54
migarashi <103835638+migarashi@users.noreply.github.com>
2022-07-16 13:34:53 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -eu
-# shellcheck disable=SC1090
 
-/bin/cp .gitconfig ~
+cp .gitconfig ~
 git config --global user.email "$1"
 
 defaults write com.apple.screencapture location ~/Downloads
@@ -11,11 +10,10 @@ ln -fs "$PWD"/.vimrc ~
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 cat < brew_formula.txt | xargs brew install
 
-/bin/cp .zshrc ~
+cp .zshrc ~
 tfenv install
 tfenv use latest
 terraform -install-autocomplete
-. ~/.zshrc
 
 etc=/Applications/Docker.app/Contents/Resources/etc
 ln -fs $etc/_docker "$(brew --prefix)"/share/zsh-completions/
