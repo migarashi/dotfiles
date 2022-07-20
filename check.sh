aed3c6f469630f43579ec2e19ec1a14ee07a9c52
migarashi <103835638+migarashi@users.noreply.github.com>
2022-07-20 13:42:29 +0900

    Update dotfiles

--- check.sh
+++ check.sh
@@ -3,3 +3,9 @@
 shellcheck ./*.sh .zshrc
 shfmt -bn -ci -i 2 -sr -w ./*.sh .zshrc
 zsh -n .zshrc
+
+if [ "$(git shortlog -ces --group=author | wc -l)" -ne 2 ] \
+  || [ "$(git log --pretty=format:%s | sort | uniq | wc -l)" -ne 2 ]; then
+  echo wrong commit. >&2
+  exit 1
+fi
