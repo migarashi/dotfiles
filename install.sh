7b39c25545ac64980484a4ae07a6dfa3aa7e03f8
migarashi <103835638+migarashi@users.noreply.github.com>
2022-07-27 14:02:35 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -eu
 
+enable_completion() {
+  ls "$1" > /dev/null
+  ln -fs "$1" "$(brew --prefix)"/share/zsh-completions/"$2"
+}
+
 cp .gitconfig ~
 git config --global user.email "$1"
 
@@ -15,8 +20,8 @@ tfenv install
 tfenv use latest
 terraform -install-autocomplete
 
-etc=/Applications/Docker.app/Contents/Resources/etc
-ln -fs $etc/_docker "$(brew --prefix)"/share/zsh-completions/
-ln -fs $etc/docker-compose.zsh-completion "$(brew --prefix)"/share/zsh-completions/_docker-compose
+path=/Applications/Docker.app/Contents/Resources/etc
+enable_completion $path/docker-compose.zsh-completion _docker-compose
+enable_completion $path/docker.zsh-completion _docker
 
 echo done!
