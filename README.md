eafb4681d0dd0a3360667e9755c51cb495f427d3
migarashi <103835638+migarashi@users.noreply.github.com>
2023-07-08 13:36:35 +0900

    Update dotfiles

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # dotfiles
 
 ```sh
-$ ./install.sh 103835638+migarashi@users.noreply.github.com && exec $SHELL -l
+% ./install.sh 103835638+migarashi@users.noreply.github.com && exec $SHELL -l
 ```
--- install.sh
+++ install.sh
@@ -5,22 +5,18 @@ enable_completion() {
   ln -fs "$1" "$2"
 }
 
+brew bundle --no-lock
+cp .vimrc .zshrc ~
 defaults write com.apple.screencapture location ~/Downloads
+git config --global core.quotepath false
+git config --global url.git@github.com:.insteadOf https://github.com/
+git config --global user.email "$1"
+git config --global user.name migarashi
 
 dst_path=~/.zsh/completion
 src_path=/Applications/Docker.app/Contents/Resources/etc
 mkdir -p $dst_path
 enable_completion $src_path/docker-compose.zsh-completion $dst_path/_docker-compose
 enable_completion $src_path/docker.zsh-completion $dst_path/_docker
-
-bash -c "$(curl -LSfs https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew bundle --no-lock
-
-cp .gitconfig .vimrc .zshrc ~
-git config --global user.email "$1"
-
-tfenv install
-tfenv use latest
-terraform -install-autocomplete
 
 echo done!
