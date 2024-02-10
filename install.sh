bf3a84d760347ba98f4343f6eaff779441a7ec63
migarashi <103835638+migarashi@users.noreply.github.com>
2024-02-10 14:51:34 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp .vimrc .zshrc ~
 defaults write com.apple.screencapture location ~/Downloads
 git config --global core.quotepath false
 git config --global url.git@github.com:.insteadOf https://github.com/
-git config --global user.email "$1"
-git config --global user.name migarashi
+git config --global user.email "${1:-103835638+migarashi@users.noreply.github.com}"
+git config --global user.name "${2:-migarashi}"
 
 echo done!
