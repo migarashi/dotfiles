c6f2b34b8797f920368c79f65ab0e1a6484284d0
migarashi <103835638+migarashi@users.noreply.github.com>
2024-10-05 20:46:55 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -eu
 
 brew bundle --no-lock
+
+[ -f "pn.mp3" ] || sox -n pn.mp3 synth 16:00:00 pinknoise
 cp .vimrc .zshrc ~
 cp settings.json ~/Library/Application\ Support/Code/User/
-defaults write com.apple.screencapture location ~/Downloads
+defaults write com.apple.screencapture location ~/Downloads/
 git config --global core.quotepath false
 git config --global url.git@github.com:.insteadOf https://github.com/
 git config --global user.email "${1:-103835638+migarashi@users.noreply.github.com}"
